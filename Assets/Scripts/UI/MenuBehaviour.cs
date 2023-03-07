e81e7b3e53b21aaac719b9944cb55d321bfb413d
FusTaFah <mfadel_1990@hotmail.com>
2023-03-07 14:48:56 +0000

    adds main menu

---
+++ Assets/Scripts/UI/MenuBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public static void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
